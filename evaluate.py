413e8b1a8a424851b40ee0c63f63f20d65ebea60
Juan Jose Suarez <jjsuarestra99@gmail.com>
2021-11-29 20:49:08 -0500

    Add test split evaluation

--- evaluate.py
+++ evaluate.py
@@ -2,11 +2,17 @@ import pandas as pd
 from joblib import load
 from sklearn.metrics import accuracy_score, f1_score
 
-df = pd.read_csv('data/features/train.csv')
-y_true = df.pop('target')
-
 clf = load('artifacts/clf.joblib')
-y_pred = clf.predict(df)
+    
+def evaluate(path):
+    print(path)
+    df = pd.read_csv(path)
+    y_true = df.pop('target')
 
-print('Accuracy', accuracy_score(y_true, y_pred))
-print('F1 Score', f1_score(y_true, y_pred))
+    y_pred = clf.predict(df)
+
+    print('Accuracy', accuracy_score(y_true, y_pred))
+    print('F1 Score', f1_score(y_true, y_pred))
+
+evaluate('data/features/train.csv')
+evaluate('data/features/test.csv')
--- normalize.py
+++ normalize.py
@@ -3,18 +3,29 @@ import pandas as pd
 from joblib import dump
 from sklearn.preprocessing import StandardScaler
 
-df = pd.read_csv('data/train_split.csv')
-y = df.pop('target')
-df.pop('id')
+df_train = pd.read_csv('data/train_split.csv')
+y_train = df_train.pop('target')
+df_train.pop('id')
+
+df_test = pd.read_csv('data/test_split.csv')
+y_test = df_test.pop('target')
+df_test.pop('id')
 
 scaler = StandardScaler()
-X = scaler.fit_transform(df)
-df = pd.DataFrame(data=X, columns=df.columns)
-df['target'] = y
+X_train = scaler.fit_transform(df_train)
+X_test = scaler.transform(df_test)
+
+df_train = pd.DataFrame(data=X_train, columns=df_train.columns)
+df_train['target'] = y_train
+
+df_test = pd.DataFrame(data=X_test, columns=df_test.columns)
+df_test['target'] = y_test
+
 
 if not os.path.exists('data/features'):
     os.mkdir('data/features')
-df.to_csv('data/features/train.csv')
+df_train.to_csv('data/features/train.csv')
+df_test.to_csv('data/features/test.csv')
 
 if not os.path.exists('artifacts/'):
     os.mkdir('artifacts/')
