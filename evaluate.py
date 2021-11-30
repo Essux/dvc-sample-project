5d50963d94ba75e0f7b6dde01bf1f08936b4c4a6
Juan Jose Suarez <jjsuarestra99@gmail.com>
2021-11-29 20:57:38 -0500

    Add metric logging

--- evaluate.py
+++ evaluate.py
@@ -1,18 +1,28 @@
+import json
 import pandas as pd
 from joblib import load
 from sklearn.metrics import accuracy_score, f1_score
 
 clf = load('artifacts/clf.joblib')
-    
-def evaluate(path):
+metrics = {}
+
+def evaluate(path, alias):
     print(path)
     df = pd.read_csv(path)
     y_true = df.pop('target')
 
     y_pred = clf.predict(df)
 
-    print('Accuracy', accuracy_score(y_true, y_pred))
-    print('F1 Score', f1_score(y_true, y_pred))
+    accuracy = accuracy_score(y_true, y_pred)
+    print('Accuracy', accuracy)
+    metrics[alias+'_accuracy'] = accuracy
 
-evaluate('data/features/train.csv')
-evaluate('data/features/test.csv')
+    f1 = f1_score(y_true, y_pred)
+    print('F1 Score', f1)
+    metrics[alias+'_f1_score'] = f1
+
+evaluate('data/features/train.csv', 'train')
+evaluate('data/features/test.csv', 'test')
+
+with open('metrics.json', 'w') as file:
+    json.dump(metrics, file)
